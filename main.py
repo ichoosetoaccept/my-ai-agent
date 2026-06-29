a706da222b979fe8a38d642496975032928fd1e9
Ismar <1242091+ichoosetoaccept@users.noreply.github.com>
2026-06-29 20:48:19 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("no api key found")
+
+client = genai.Client(api_key=api_key)
+interaction = client.models.generate_content(
+    model="gemini-3.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+
+print(interaction.text)
