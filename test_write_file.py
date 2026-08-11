41eabaa968bbc9e20dec88f06b24c0fa0dea3537
Ismar <1242091+ichoosetoaccept@users.noreply.github.com>
2026-08-11 07:50:41 +0200

    fix tests for write file

--- test_write_file.py
+++ test_write_file.py
@@ -1,10 +1,10 @@
 from functions.write_file_content import write_file
 
 print("Write lorem ipsum 1")
-write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
 print("Write lorem ipsum 2")
-write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
 print("Not allowed")
-write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
